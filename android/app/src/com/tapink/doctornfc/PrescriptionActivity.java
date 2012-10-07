f93cf67f396484236fc75b675a6ef4f847d4718c
David Young-Chan Kay <davidykay@gmail.com>
2012-10-07 16:26:07 -0400

    now successfully starting pill reminders

--- android/app/src/com/tapink/doctornfc/PrescriptionActivity.java
+++ android/app/src/com/tapink/doctornfc/PrescriptionActivity.java
@@ -1,11 +1,11 @@
 package com.tapink.doctornfc;
 
+import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
-import android.app.Activity;
 import android.os.Bundle;
 
 @ContentView(R.layout.activity_prescription)
-public class PrescriptionActivity extends Activity {
+public class PrescriptionActivity extends RoboActivity {
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
