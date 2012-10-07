e6ad851a7b038660221e7e5e6133193e805d864a
David Young-Chan Kay <davidykay@gmail.com>
2012-10-07 14:38:52 -0400

    basic prescription placeholder in place.

--- android/app/src/com/tapink/doctornfc/MedicationListActivity.java
+++ android/app/src/com/tapink/doctornfc/MedicationListActivity.java
@@ -1,7 +1,9 @@
 package com.tapink.doctornfc;
 
 import roboguice.activity.RoboListActivity;
+import roboguice.inject.ContentView;
 import roboguice.inject.InjectView;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -10,21 +12,22 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+@ContentView(R.layout.activity_prescription_list)
 public class MedicationListActivity extends RoboListActivity {
 
-  @InjectView(R.id.prescription_search) EditText mSearchText;
+  @InjectView(R.id.prescription_search)
+  EditText mSearchText;
 
   private ArrayAdapter<String> mAdapter;
 
   @Override
   public void onCreate(Bundle icicle) {
     super.onCreate(icicle);
-    setContentView(R.layout.activity_prescribe);
 
     mSearchText.addTextChangedListener(mFilterTextWatcher);
 
-
-    mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Constants.MEDICATIONS);
+    mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+        Constants.MEDICATIONS);
     setListAdapter(mAdapter);
     getListView().setTextFilterEnabled(true);
   }
@@ -33,12 +36,10 @@ public class MedicationListActivity extends RoboListActivity {
     public void afterTextChanged(Editable s) {
     }
 
-    public void beforeTextChanged(CharSequence s, int start, int count,
-                                  int after) {
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
     }
 
-    public void onTextChanged(CharSequence s, int start, int before,
-                              int count) {
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
       mAdapter.getFilter().filter(s);
     }
 
@@ -46,12 +47,11 @@ public class MedicationListActivity extends RoboListActivity {
 
   @Override
   protected void onListItemClick(ListView l, View v, int position, long id) {
-    //super.onListItemClick(l, v, position, id);
-
     String drugName = mAdapter.getItem(position);
+    Intent intent = new Intent(this, PrescribeActivity.class);
+    intent.putExtra("DRUG_NAME", drugName);
 
-
+    startActivity(intent);
   }
-
 
 }
